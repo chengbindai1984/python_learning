49f156a3c64ea198e6166a50f1707ef378ae7794
Dai <steven.dai@mkcorp.com>
2017-10-09 13:25:53 +0800

    printTable completed

--- stringProcess_printTable.py
+++ stringProcess_printTable.py
@@ -1,30 +1,19 @@
-
-
+from __future__ import print_function
 rowData = [['apple', 'organge111','cherries','banana'], [
     'alice', 'bob', 'Carol3333', 'David'], ['dog', 'cats888', 'moose', 'goose']]
 
-#colWidths = [0] * len(tableData)
-
-# for x in range(len(tableData)):
-#print (x)
-#    for y in range(len(tableData[x])):
-#        if len(tableData[x][y])>len(tableData[x][y-1]):
-#            colWidths[x]=len(tableData[x][y])
-#        else:
-#            colWidths[x]=len(tableData[x][0])
-#print (colWidths)
-
-
 def printTable(tableData):
     colWidths = [0] * len(tableData)
-    print (colWidths)
-    for x in range(len(tableData)):
-        for y in range(len(tableData[x])):
-            if len(tableData[x][y]) > len(tableData[x][y - 1]):
-                colWidths[x] = len(tableData[x][y])
-                print (colWidths[x])
-            else:
-                colWidths[x] = len(tableData[x][y-1])
-                print (colWidths[x])
-            #print (tableData[0][y].rjust(colWidths[0], '-'))
+    for raw in range(len(tableData)):
+        compare_list=[]
+        for col in tableData[raw]: 
+            compare_list.append(len(col))
+        colWidths[raw]=max(compare_list)
+    
+    # for raw in range(len(tableData)):
+    for col in range(len(tableData[0])):
+        for raw in range(len(tableData)):
+            print (tableData[raw][col].rjust(colWidths[raw], ' '), end='  ')
+        print ('\n')
+    
 printTable(rowData)
